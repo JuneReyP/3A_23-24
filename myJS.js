c941a78c2d6bbf13e7f2a5401420891e4b774474
JuneRey <junereypalabrica@gmail.com>
2023-10-23 15:28:34 +0800

    additional topics

--- myJS.js
+++ myJS.js
@@ -169,28 +169,81 @@ function login() {
             // code block
 } 
  */
-var expr = 'sum';
+// var expr = 'sum';
 var answer;
 var num1 = 4;
 var num2 = 3;
 
-switch(expr){
-    case "sum":
-        answer = num1 + num2;
-        alert(answer);
-        break;
-    case "subtract":
-        answer = num1 - num2;
-        alert(answer);
-        break;
-    case "multiply":
-        answer = num1 * num2;
-        alert(answer);
-        break;
-    case "divide":
-        answer = num1 / num2;
-        alert(answer);
-        break;
-    default:
-        alert("Please enter a valid operator.");
+// switch(expr){
+//     case "sum":
+//         answer = num1 + num2;
+//         alert(answer);
+//         break;
+//     case "subtract":
+//         answer = num1 - num2;
+//         alert(answer);
+//         break;
+//     case "multiply":
+//         answer = num1 * num2;
+//         alert(answer);
+//         break;
+//     case "divide":
+//         answer = num1 / num2;
+//         alert(answer);
+//         break;
+//     default:
+//         alert("Please enter a valid operator.");
+// }
+
+// loops
+// for-loop
+
+//indexed-array
+var cars = ["toyota", "honda", "BMW", "toyota", "honda", "BMW"];// 0 == toyota, 1 == honda, 2 == BMW
+let person = ["Juan", 24, "Male", "CPSU"];
+
+let myArr = [
+    [
+        ["toyota", "honda", "BMW"],
+        ["toyota", "honda", "BMW"],
+        ["toyota", "honda", "BMW"]
+    ], //0
+    [
+        ["Juan", 24, "Male"],
+        ["Juan", 24, "Male"],
+        ["Juan", 24, "Male"]
+    ], //1
+    [
+        ["banana", "grapes", "apple","banana", "grapes", "apple"],
+        ["banana", "grapes", "apple"],
+        ["banana", "grapes", "apple"],
+        ["banana", "grapes", "apple"],
+        ["banana", "grapes", "apple"]
+    ] //2
+];
+//document.write(myArr[1][0]);
+
+// document.write(cars[0] + ' ');
+// document.write(cars[1] + ' ');
+// document.write(cars[2] + ' ');
+
+for(let row = 0; row < myArr.length; row++){
+    //document.write(myArr[][][] + ' ');
+    for(let col = 0; col < myArr[row].length; col++ ){
+        for(let data = 0; data < myArr[row][col].length; data++){
+            document.write(myArr[row][col][data] + ' ');
+        }
+        document.write('<br>');
+    }
+    document.write('<br>');
+}
+
+//myArr[0][0][] == toyota
+
+//while loop
+// alert(cars.length);
+let i = 0;
+while(i < cars.length){
+    document.write(cars[i] + ' ');
+    i++;
 }
